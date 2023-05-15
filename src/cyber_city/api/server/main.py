fc047fd6c36350674b283a399b6a59b66d6bcfd3
Benjamin Bowles <benjamin.d.bowles@gmail.com>
2023-05-15 11:18:12 -0500

    fixed linting

--- src/cyber_city/api/server/main.py
+++ src/cyber_city/api/server/main.py
@@ -1,6 +1,16 @@
+"""
+Modbus test web server dashboard
+
+requirements:
+- pymodbus
+- flask
+
+to run the server:
+- `flask --app ./cyber_city/api/server/main.py run'
+"""
+
 from flask import Flask, render_template, request
 from pymodbus.client import ModbusTcpClient as ModbusClient
-import json
 
 app = Flask(__name__)
 
@@ -8,32 +18,48 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/<max_coils>")
 @app.route("/<max_coils>/<ip>")
-def main(ip="127.0.0.1", max_coils=50):
+def main(ip_address="127.0.0.1", max_coils=50) -> str:
+    """
+    the main dashboard route for the web server
+
+    Args:
+        ip (str, optional): _description_. Defaults to "127.0.0.1".
+        max_coils (int, optional): _description_. Defaults to 50.
+
+    Returns:
+        (str): html result
+    """
     coils = {}
-    max = int(max_coils)
-    for i in range(0, max + 1):
+    max_coils = int(max_coils)
+    for i in range(0, max_coils + 1):
         coils[i] = False
-    return render_template("dashboard.html", ip=ip, coils=coils)
+    return render_template("dashboard.html", ip=ip_address, coils=coils)
 
 
 @app.route("/coil", methods=["POST", "GET"])
-def coil():
-    ip = request.headers["ip"]
-    coil = request.headers["coil"]
+def coil() -> str:
+    """
+    the api side of the dashboard web server
+
+    Returns:
+        (str): method type
+    """
+    ip_address = request.headers["ip"]
+    current_coil = request.headers["coil"]
     if request.method == "POST":
         value = request.get_json()["value"]
-        client = ModbusClient(ip)
+        client = ModbusClient(ip_address)
         if client.connect():
-            client.write_coil(int(coil), value)
+            client.write_coil(int(current_coil), value)
             client.close()
         return "POST"
     if request.method == "GET":
-        client = ModbusClient(ip)
+        client = ModbusClient(ip_address)
         if client.connect():
-            result = client.read_coils(int(coil), 1).bits[0]
+            result = client.read_coils(int(current_coil), 1).bits[0]
             client.close()
             return {
-                "coil": int(coil),
+                "coil": int(current_coil),
                 "value": result,
             }
         return "GET"
