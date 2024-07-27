648b3011bb47d2887f2a636709b299750ef8d321
Benjamin Bowles <benjamin.d.bowles@gmail.com>
2024-07-27 09:20:57 -0500

    clean

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from module import *
+from module import Game, Role, load_game_data
 
 game = Game(*load_game_data("data.json"))
 game.fill_hands()
 
-print(game.players[Role.OFFENSE])
 print(game.players[Role.DEFENSE])
-print("\n".join(map(str, game.districts)))
+print(game.players[Role.OFFENSE])
+print("".join(str(d) for d in game.districts))
--- src/module/game.py
+++ src/module/game.py
@@ -5,15 +5,6 @@ from typing import List, Tuple
 
 
 class Role(Enum):
-    """
-    Enum representing possible roles in the game for players, cards, or districts.
-
-    Attributes:
-        UNKNOWN (Enum): Represents an undefined role.
-        OFFENSE (Enum): Represents an offensive role.
-        DEFENSE (Enum): Represents a defensive role.
-    """
-
     UNKNOWN = auto()
     OFFENSE = auto()
     DEFENSE = auto()
@@ -23,20 +14,6 @@ class Role(Enum):
 
 
 class Player:
-    """
-    Represents a player in the game, managing their role, hand of cards, score points, and
-    readiness status.
-
-    Attributes:
-        `role` (Role): Role of the player, either offensive or defensive.
-        `hand` (List[Card]): List of cards in the player's hand.
-        `points` (int): Points scored by the player.
-        `ready` (bool): Indicates whether the player is ready to play a card
-
-    Constants:
-        MAX_HAND (int): Maximum number of cards a player can hold.
-    """
-
     MAX_HAND = 5
 
     def __init__(self, role: Role):
@@ -46,21 +23,11 @@ class Player:
         self.ready: bool = False
 
     def __str__(self) -> str:
-        return f"Player: {self.role} (Points: {self.points}, Ready: {self.ready}) \nHand: {', '.join(map(str, self.hand))}\n"
+        hand = ", ".join(map(str, self.hand))
+        return f"Player {self.role}: {self.points} pts, Ready: {self.ready} \nHand: {hand} \n"
 
 
 class Card:
-    """
-    Represents a card in the game, characterized by its role, name, value, and the odds of drawing
-    it.
-
-    Attributes:
-        `role` (Role): Role of the card, either offensive or defensive.
-        `name` (str): Name of the card.
-        `value` (int): Value of the card.
-        `odds` (float): Odds of drawing the card.
-    """
-
     def __init__(self, role: Role, name: str, value: int, odds: float):
         self.role = role
         self.name = name
@@ -72,39 +39,21 @@ class Card:
 
 
 class District:
-    """
-    Represents a district in the game, which can be owned by players based on offensive and
-    defensive card plays.
-
-    Attributes:
-        `name` (str): Name of the district.
-        `owner` (Role): Current owner of the district, defaults to Role.UNKNOWN.
-        `levels` (dict): Tracks the cumulative levels of offense and defense in the district.
-    """
-
     def __init__(self, name: str):
         self.name = name
         self.owner: Role = Role.UNKNOWN
         self.levels: dict = {Role.OFFENSE: 0, Role.DEFENSE: 0}
 
     def __str__(self) -> str:
-        return f"{self.name} (Owner: {self.owner}, Offense: {self.levels[Role.OFFENSE]}, Defense: {self.levels[Role.DEFENSE]})"
+        return (
+            f"{self.name}: Owner={self.owner}\n"
+            f"Off={self.levels[Role.OFFENSE]}, Def={self.levels[Role.DEFENSE]}\n"
+        )
 
     def add_card(self, card: Card) -> None:
-        """
-        Adds the value of the card to the corresponding level in the district. The card's role
-        determines whether the value is added to offense or defense.
-
-        Args:
-            card (Card): The card to be added to the district.
-        """
         self.levels[card.role] += card.value
 
     def calculate_owner(self) -> None:
-        """
-        Determines the owner of the district based on the levels of offense and defense. If the
-        levels are equal, the district remains or becomes unowned.
-        """
         o, d = self.levels[Role.OFFENSE], self.levels[Role.DEFENSE]
         new_owner = (
             Role.UNKNOWN if o == d else (Role.OFFENSE if o > d else Role.DEFENSE)
@@ -119,21 +68,6 @@ class District:
 
 
 class Game:
-    """
-    This class represents the game itself. It contains the players, cards, districts, and the
-    current round. The game logic is implemented in the `play_round` method, which should be
-    overriden by a subclass.
-
-    Attributes:
-        `players` (dict): Dictionary containing the offensive and defensive players.
-        `districts` (List[District]): List of districts in the game.
-        `cards` (List[Card]): List of cards in the game.
-        `current_round` (int): Current round number.
-
-    Constants:
-        MAX_ROUNDS (int): Maximum number of rounds in the game.
-    """
-
     MAX_ROUNDS = 10
 
     def __init__(self, cards: List[Card], districts: List[District]):
@@ -148,84 +82,57 @@ class Game:
         self.current_round: int = 1
 
     def new_card(self, role: Role) -> Card:
-        """
-        Randomly selects a card based on the role and the odds of drawing it. The odds are used as
-        weights for the random selection.
-
-        Args:
-            role (Role): Role of the card to be drawn.
-
-        Returns:
-            Card: The randomly selected card.
-        """
         eligible = [card for card in self.cards if card.role == role]
         return random.choices(eligible, weights=[card.odds for card in eligible])[0]
 
     def fill_hands(self) -> None:
-        """
-        For each player, fill their hand with cards until it reaches the maximum hand size.
-        """
         for player in self.players.values():
             while len(player.hand) < Player.MAX_HAND:
                 player.hand.append(self.new_card(player.role))
 
     def update_districts(self) -> None:
-        """
-        For each district, run the `calculate_owner` method to determine the owner based on the
-        levels of offense and defense in the district.
-        """
         for district in self.districts:
             district.calculate_owner()
 
     def update_points(self) -> None:
-        """
-        Check the owner of each district and award points to the corresponding player. If the
-        district is unowned, no points are awarded.
-        """
         for district in self.districts:
             if district.owner in self.players:
                 self.players[district.owner].points += 1
 
     def play_round(self) -> None:
-        """
-        TODO: Placeholder method that should be overriden by a subclass to implement game logic.
+        pass
 
-        This method should handle the players' card plays, set the readiness status, and update the
-        districts' levels based on the played cards when both players are ready.
-
-        The current implementation does nothing.
-        """
+    def play_card(self, player: Player, card: Card, district: District) -> None:
+        player.hand.remove(card)
+        district.add_card(card)
 
     def start_game(self) -> None | Role:
-        """
-        Starts the game and plays rounds until the maximum number of rounds is reached. The winner
-        is then determined based on the points scored by the players and printed to the console.
-
-        It may be beneficial to override this method in a subclass to implement custom game logic,
-        such as handling player input or displaying game state to the user.
-
-        Returns:
-            None | Role: In this implementation, the role of the winning player is returned.
-        """
         while self.current_round <= Game.MAX_ROUNDS:
             self.players[Role.OFFENSE].ready = False
             self.players[Role.DEFENSE].ready = False
+            self.fill_hands()
 
             self.play_round()
+
             self.update_districts()
             self.update_points()
             self.current_round += 1
 
         return self.determine_winner()
 
-    def determine_winner(self) -> Role:
-        """
-        Determines the game winner based on points. If the points are equal, the winner is
-        undetermined.
+    def reset_game(self) -> None:
+        for player in self.players.values():
+            player.points = 0
+            player.hand = []
+            player.ready = False
 
-        Returns:
-            Role: The role of the winning player or Role.UNKNOWN if the game is a tie.
-        """
+        for district in self.districts:
+            district.owner = Role.UNKNOWN
+            district.levels = {Role.OFFENSE: 0, Role.DEFENSE: 0}
+
+        self.current_round = 1
+
+    def determine_winner(self) -> Role:
         if self.players[Role.OFFENSE].points > self.players[Role.DEFENSE].points:
             return Role.OFFENSE
         elif self.players[Role.OFFENSE].points < self.players[Role.DEFENSE].points:
@@ -235,16 +142,6 @@ class Game:
 
 
 def load_game_data(filepath: str) -> Tuple[List[Card], List[District]]:
-    """
-    Loads game data from a specified JSON file containing card and district details.
-
-    Args:
-        filepath (str): Path to the JSON file.
-
-    Returns:
-        Tuple containing a list of Card objects and a list of District objects, or None for each if
-        loading fails.
-    """
     try:
         with open(filepath, "r", encoding="utf-8") as file:
             data = json.load(file)
